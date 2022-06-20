9d9bbdaebcaf10328579805b6030ce0c45c39bbf
Reese <86006893+Spyabo@users.noreply.github.com>
2022-06-20 23:43:24 +0100

    making it more efficient

--- Two Pointer/Valid Palidrome.py
+++ Two Pointer/Valid Palidrome.py
@@ -1,12 +1,8 @@
 def isPalindrome(s):
-    string = s.lower()
-
-    import re
-    string = re.sub(r'[^a-zA-Z0-9]', '', string)
-
-    if string == string[::-1]:
-        return True
-    else:
-        return False
+  s = re.sub(r'[^a-zA-Z0-9]', '', s.lower())
+  if s == s[::-1]:
+    return True
+  else:
+    return False
 
 isPalindrome(s='"A man, a plan, a canal: Panama"')
